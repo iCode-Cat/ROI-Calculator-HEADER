f86a159700cdf34e616e1ccdf0ce534c22fd31aa
iCode-Cat <devranboyac8@gmail.com>
2021-09-14 01:23:00 +0300

    Bug fixes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Collapsible from './Collapsible';
 import TabMenu from './TabMenu';
